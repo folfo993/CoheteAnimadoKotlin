972c92308acbf80c57ef2c91afb4c824fd374aa4
FOLFO93 <uv.adolfo@hotmail.com>
2018-05-21 17:23:36 +0200

    Añadida funcionalidad del boton; llamada al segundo fragment(funcionando)

--- app/src/main/java/com/example/folfo93/kotlinanimado/MainActivity.kt
+++ app/src/main/java/com/example/folfo93/kotlinanimado/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.folfo93.kotlinanimado
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_main)
+
+        start_button.setOnClickListener {
+            val datos = Intent(this, AnimationActivity::class.java)
+            datos.putExtra("key1", "valor1")
+            startActivity(datos)
+        }
     }
+
+
 }
