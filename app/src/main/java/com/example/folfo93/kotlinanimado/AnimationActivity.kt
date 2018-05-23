032804e02bfe13532390fc02c7c41b18e8f4a153
FOLFO93 <uv.adolfo@hotmail.com>
2018-05-23 17:04:52 +0200

    Commit final. Proyecto terminado y funcionando (añadidos comentarios)

--- app/src/main/java/com/example/folfo93/kotlinanimado/AnimationActivity.kt
+++ app/src/main/java/com/example/folfo93/kotlinanimado/AnimationActivity.kt
@@ -13,6 +13,8 @@ import kotlinx.android.synthetic.main.fragment_animation.view.*
 
 class AnimationActivity : AppCompatActivity() {
 
+    //Activity que se encarga de la animacion
+    
     protected lateinit var calendar: View
     protected lateinit var frameLayout: View
     protected var screenHeight = 10f
@@ -36,9 +38,11 @@ class AnimationActivity : AppCompatActivity() {
         screenHeight = displayMetrics.heightPixels.toFloat()
     }
 
+    //Funcion de la animacion
     private fun onStartAnimation(){
         val valueAnimator = ValueAnimator.ofFloat(100f, -screenHeight)
 
+        //La animacion se desplazara por el eje Y (de abajo hacia arriba)
         valueAnimator.addUpdateListener {
             val value = it.animatedValue as Float
             calendar.translationY = value
@@ -50,6 +54,7 @@ class AnimationActivity : AppCompatActivity() {
         valueAnimator.start()
     }
 
+    //Companion object que indica a la funcion la duracion de la animacion
     companion object {
         val DEFAULT_ANIMATION_DURATION = 1700L
     }
--- app/src/main/java/com/example/folfo93/kotlinanimado/MainActivity.kt
+++ app/src/main/java/com/example/folfo93/kotlinanimado/MainActivity.kt
@@ -8,10 +8,12 @@ import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //Activity principal que contiene la llamada al fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_main)
 
+        //Añadimos funcionalidad al boton que llamara a la segunda activity
         start_button.setOnClickListener {
             val datos = Intent(this, AnimationActivity::class.java)
             datos.putExtra("key1", "valor1")
